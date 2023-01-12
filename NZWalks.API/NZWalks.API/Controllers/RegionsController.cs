321b0b26bfe81aaed7cff23d1b764dd00f1de9ac
anuraggope <anuraggope19@gmail.com>
2023-01-13 02:17:27 +0530

    irepo an repo

---
+++ NZWalks.API/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+
+        public RegionsController(IRegionRepository regionRepository)
+        {
+            this.regionRepository=regionRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = regionRepository.getData();
+            return Ok(regions);
+        }
+    }
+}
